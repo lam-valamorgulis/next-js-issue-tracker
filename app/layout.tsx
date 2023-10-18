128db08c261bb1af732161f59527dc2b4d1c637e
Lam <80387108+lam-valamorgulis@users.noreply.github.com>
2023-10-18 12:53:44 +0000

    Update 7 files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,21 @@
 //  Nextjs 13 wrap your pages with a layout component, providing a consistent structure across those pages.
 //Next.js uses a file-system based router where folders are used to define routes.
+//The most common are pages to show UI unique to a route, and layouts to show UI that is shared across multiple routes.
 
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+//lamdang0306 - SumxYzOW0LfWLbaA
+//mongodb+srv://lamdang0306:<password>@cluster0.abccnyu.mongodb.net/?retryWrites=true&w=majority
 
-import NavBar from './NavBar';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+import NavBar from "./NavBar";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Issue Tracker',
-  description: 'Learning Nextjs 13',
+  title: "Issue Tracker",
+  description: "Learning Nextjs 13",
 };
 
 export default function RootLayout({
